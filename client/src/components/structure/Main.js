61f4729aa6f7764a169ce11522b07914f8c066dc
rosadolson <rosa.dolson@gmail.com>
2018-02-06 21:15:08 -0700

    sign up page added

--- client/src/components/structure/Main.js
+++ client/src/components/structure/Main.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
+import PropTypes from 'prop-types'
 import Home from '../pages/Home'
 import About from '../pages/About'
+import SignUpContainer from '../pages/authentication/SignUpContainer'
 
-const Main = () => {
+const Main = ({ domainData }) => {
   return (
     <div>
       <Route exact path='/' component={Home} />
       <Route path='/about' component={About} />
+      <Route path='/sign-up'render={() => <SignUpContainer domainData={domainData} />} />
     </div>
   )
+}
+
+Main.propTypes = {
+  domainData: PropTypes.object.isRequired
 }
 
 export default Main
