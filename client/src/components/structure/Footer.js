4763f39736725634d06639cfb7b9960288480968
Antonio Guzman <alguzman1@yahoo.com>
2018-02-08 20:29:29 -0500

    updated footer

--- client/src/components/structure/Footer.js
+++ client/src/components/structure/Footer.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const styles = {
   container: {
-    background: '#87fffb'
+    background: '#87fffb',
+    display: 'flex',
+    justifyContent: 'center'
   }
 }
 
 const Footer = () => {
   return (
     <div style={styles.container} >
-      <p>F2F</p>
+      <h3>F2F</h3>
     </div>
   )
 }
