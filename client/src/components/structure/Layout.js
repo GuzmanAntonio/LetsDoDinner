b12097895dacf87bc326d97c08d8915ad47370f5
Antonio Guzman <alguzman1@yahoo.com>
2018-02-13 23:32:05 -0500

    Added menu to the menu button.  Deleted Header Menu

--- client/src/components/structure/Layout.js
+++ client/src/components/structure/Layout.js
@@ -2,20 +2,24 @@ import React from 'react'
 import Menu from './header/Menu'
 import Main from './Main'
 import Footer from './Footer'
-import Header from '../structure/header/Header'
 
 const style = {
   container: {
     backgroundColor: '#87fffb',
     textAlign: 'center'
+  },
+  menu: {
+    display: 'flex',
+    flexDirection: 'row'
   }
 }
 
 const Layout = () => {
   return (
     <div style={style.container} >
-      <Header />
-      <Menu />
+      <div style={style.menu} >
+        <Menu />
+      </div>
       <div>
         <Main />
       </div>
